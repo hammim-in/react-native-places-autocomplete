c8978cc485c262b1ec2cf764c83b6704d793cac9
Ham Mim <hammim.in@Hams-MacBook-Pro.local>
2025-04-29 08:05:40 +0530

    place details added

--- dist/PlacesAutocomplete.d.ts
+++ dist/PlacesAutocomplete.d.ts
@@ -1,9 +1,30 @@
 import * as React from 'react';
+import { StyleProp, ViewStyle, TextStyle } from 'react-native';
 interface GooglePlacePrediction {
     description: string;
     place_id: string;
     reference: string;
     id: string;
+    matched_substrings?: Array<{
+        length: number;
+        offset: number;
+    }>;
+    structured_formatting?: any;
+    terms?: Array<{
+        offset: number;
+        value: number;
+    }>;
+    types?: Array<string>;
+    result?: {
+        address_components: Array<any>;
+        geometry: {
+            location: {
+                lat: number;
+                lng: number;
+            };
+        };
+        formatted_address: string | any;
+    };
 }
 interface PlacesQuery {
     sessiontoken?: string;
@@ -22,6 +43,12 @@ interface PlacesAutocompleteProps {
     minLength?: number;
     debounce?: number;
     onPlaceSelected?: (place: GooglePlacePrediction) => void;
+    styles?: {
+        container?: StyleProp<ViewStyle>;
+        inputRow?: StyleProp<ViewStyle>;
+        textInput?: StyleProp<TextStyle>;
+    };
+    placeDetailsEnable: boolean;
 }
 declare const PlacesAutocomplete: React.FC<PlacesAutocompleteProps>;
 export default PlacesAutocomplete;
--- dist/PlacesAutocomplete.js
+++ dist/PlacesAutocomplete.js
@@ -7,9 +7,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 import * as React from 'react';
 import { View, TextInput, FlatList, Text, TouchableOpacity, ActivityIndicator, StyleSheet } from 'react-native';
-const PlacesAutocomplete = ({ query, apiKey, placeholder = 'Search places...', minLength = 3, debounce = 500, onPlaceSelected, }) => {
+const GOOGLE_AUTO_COMPLETE_API = `https://maps.googleapis.com/maps/api/place/autocomplete/json`;
+const GOOGLE_PLACE_DETAILS_API = `https://maps.googleapis.com/maps/api/place/details/json`;
+const PlacesAutocomplete = (_a) => {
+    var { query, apiKey, placeholder = 'Search places...', minLength = 3, debounce = 500, onPlaceSelected, placeDetailsEnable = false } = _a, props = __rest(_a, ["query", "apiKey", "placeholder", "minLength", "debounce", "onPlaceSelected", "placeDetailsEnable"]);
     const [inputText, setInputText] = React.useState('');
     const [predictions, setPredictions] = React.useState([]);
     const [loading, setLoading] = React.useState(false);
@@ -26,10 +40,9 @@ const PlacesAutocomplete = ({ query, apiKey, placeholder = 'Search places...', m
                 .filter(key => params[key] !== undefined)
                 .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
                 .join('&');
-            const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?${queryString}&key=${apiKey}`;
-            const response = yield fetch(url);
-            const data = yield response.json();
-            setPredictions(data.predictions || []);
+            const placeListRes = yield fetch(`${GOOGLE_AUTO_COMPLETE_API}?${queryString}&key=${apiKey}`);
+            const placeListJson = yield placeListRes.json();
+            setPredictions(placeListJson.predictions || []);
         }
         catch (error) {
             console.error('Error fetching predictions:', error);
@@ -52,19 +65,25 @@ const PlacesAutocomplete = ({ query, apiKey, placeholder = 'Search places...', m
         setInputText('');
         setPredictions([]);
     };
-    const handlePlacePress = (place) => {
+    const handlePlacePress = (place) => __awaiter(void 0, void 0, void 0, function* () {
         setInputText(place.description);
         setPredictions([]);
         if (onPlaceSelected) {
-            onPlaceSelected(place);
+            if (placeDetailsEnable) {
+                const placeDetailsRes = yield fetch(`${GOOGLE_PLACE_DETAILS_API}?placeid=${place.place_id}&key=${apiKey}`);
+                const placeDetailsJson = yield placeDetailsRes.json();
+                onPlaceSelected(Object.assign(Object.assign({}, place), { result: placeDetailsJson.result }));
+                return;
+            }
+            onPlaceSelected(Object.assign({}, place));
         }
-    };
-    return (React.createElement(View, { style: styles.container },
-        React.createElement(View, { style: styles.inputRow },
-            React.createElement(TextInput, { value: inputText, onChangeText: handleInputChange, placeholder: placeholder, placeholderTextColor: "#888", style: styles.textInput }),
-            inputText.length > 0 && !loading && (React.createElement(TouchableOpacity, { onPress: handleClear, style: styles.clearButton },
+    });
+    return (React.createElement(View, { style: [styles.container, props.styles.container] },
+        React.createElement(View, { style: [styles.inputRow, props.styles.inputRow] },
+            React.createElement(TextInput, { value: inputText, onChangeText: handleInputChange, placeholder: placeholder, placeholderTextColor: "#888", style: [styles.textInput, props.styles.textInput] }),
+            inputText.length > 0 && !loading && (React.createElement(TouchableOpacity, { onPress: handleClear },
                 React.createElement(Text, { style: styles.clearButtonText }, "\u2716\uFE0F"))),
-            loading && React.createElement(ActivityIndicator, { size: "small", color: "#888", style: styles.loader })),
+            loading && React.createElement(ActivityIndicator, { size: "small", color: "#888" })),
         React.createElement(FlatList, { data: predictions, keyExtractor: (item) => item.place_id, renderItem: ({ item }) => (React.createElement(TouchableOpacity, { onPress: () => handlePlacePress(item), style: styles.suggestionItem },
                 React.createElement(Text, { style: styles.suggestionText }, item.description))), style: styles.suggestionsList, keyboardShouldPersistTaps: "handled" })));
 };
@@ -74,24 +93,19 @@ const styles = StyleSheet.create({
     },
     inputRow: {
         flexDirection: 'row',
+        justifyContent: "space-between",
         alignItems: 'center',
         paddingHorizontal: 8,
     },
     textInput: {
-        flex: 1,
+        // flex: 1,
         fontSize: 16,
         color: '#333',
         paddingVertical: 10,
     },
-    clearButton: {
-        paddingHorizontal: 8,
-    },
     clearButtonText: {
         fontSize: 16,
         color: '#888',
-    },
-    loader: {
-        marginLeft: 8,
     },
     suggestionsList: {
         marginTop: 8,
